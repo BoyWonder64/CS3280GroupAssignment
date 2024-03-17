0ca36e551381961c99bb4c9b5a0fbf20cd39930c
BoyWonder64 <94984524+BoyWonder64@users.noreply.github.com>
2024-03-17 14:43:51 -0600

    Added Folders

--- Main/clsMainLogic.cs
+++ Main/clsMainLogic.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GroupAssignment
+namespace GroupAssignment.Main
 {
-    internal class clsMainSQL
+    internal class clsMainLogic
     {
     }
 }
