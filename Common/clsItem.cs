c0215ae6f717418ff32598326da03a1c4b8068ab
BoyWonder64 <94984524+BoyWonder64@users.noreply.github.com>
2024-03-17 21:56:24 -0600

    Added the x86 CPU option along with its respective folder I also added the Invoice.mdb

---
+++ Common/clsItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment.Common
+{
+    public class clsItem
+    {
+        /// <summary>
+        /// Placeholder for Item Code
+        /// </summary>
+        private string sItemCode;
+
+        /// <summary>
+        /// Placeholder for Item Desc
+        /// </summary>
+        private string sItemDesc;
+
+        /// <summary>
+        /// Placeholder for ItemCost
+        /// </summary>
+        private string sItemCost;
+
+        /// <summary>
+        /// ItemCode getter and setter
+        /// </summary>
+        public string ItemCode { get {  return sItemCode; } set {  sItemCode = value; } }
+
+        /// <summary>
+        /// ItemDesc getter and setter
+        /// </summary>
+        public string ItemDesc { get {  return sItemDesc; } set { sItemDesc = value; } }
+
+        /// <summary>
+        /// Item Cost getter and setter
+        /// </summary>
+        public string ItemCost { get {  return sItemCost; } set { sItemCost = value; } }
+     
+    }
+}
--- Items/clsItemsLogic.cs
+++ Items/clsItemsLogic.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GroupAssignment.Common;
 
 namespace GroupAssignment.Items
 {
     /// <summary>
     /// Contains the business logic for the Items Screen
     /// </summary>
-    class clsItemsLogic
+    public class clsItemsLogic
     {
- 
+        private List<clsItem> ItemList = new List<clsItem>();
+
+        public List<clsItem> GetAllItems()
+        {
+            return ItemList;
+        }
+        //GetAllItems returns List<clsItem>
+        //AddItem(clsItem)
+        //EditItem(clsItem clsOldItem, clsItem clsNewItem)
+        //DeleteItem(clsItem clsItemtoDelete)
+        //IsItemOnInvoice(clsItem)
     }
 }
