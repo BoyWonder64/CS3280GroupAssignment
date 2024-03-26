0549c6dc92c33a1aa505519b9ea3a14b4c37dba9
Joshua Sorensen <163444455+joshjosh0424@users.noreply.github.com>
2024-03-26 13:34:05 -0600

    Update clsSearchLogic.cs

--- Search/clsSearchLogic.cs
+++ Search/clsSearchLogic.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using GroupAssignment.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,18 @@ namespace GroupAssignment.Search
 {
     class clsSearchLogic
     {
+
+        /// <summary>
+        /// THis will be an ivoice to be added
+        /// </summary>
+        clsInvoice Invoice;
+
+        //GetInvoices(InvoiceNumber, InvoiceDate, TotalCost)
+
+        //GetDistinctInvoiceNum(clsInvoice DisNum) {}
+
+        //GetDistinctInvoiceDate(clsInvoice DisDate) {}
+
+        //GetDistinctTotalCost(clsInvoice DistCost) {}
     }
 }
