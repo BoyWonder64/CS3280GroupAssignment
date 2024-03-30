98b329cc0affa972a36d471df8631e6823140a2f
Joshua Sorensen <joshuasorensen1@mail.weber.edu>
2024-03-29 19:47:23 -0600

    created notes

--- Search/wndSearch.xaml.cs
+++ Search/wndSearch.xaml.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace GroupAssignment
 {
@@ -77,5 +79,21 @@ namespace GroupAssignment
         {
             this.Hide();
         }
+
+        //USER WILL SELECT AN INVOICE AND THE INVOICE ID WILL HAVE TO BE SENT BACK TO THE MAIN WINDOW,
+        //CREATE A (PROPERTY) THAT THE MAIN WINDOW CAN CHECK TO SEE IF AN INVOICE WAS ACTUALLY SELECTED,
+        //SO THAT WE CAN LOAD THAT INVOICE INTO THE MAIN WINDOW.
+
+
+        //SELECT INVOICE CLICK(NEEDS TO SET A LOCAL VARIABLE THAT HOLDS THE INVOICE ID, THEN THE MAIN
+        //WINDOW CAN ACCES THAT VIA A PROPERTY AND WILL KNOW WETHER IT HAS BEEN SELECTED SO WE CAN JUMP
+        //TO THE MAIN WINDOW.
+
+
+        //PASS IN WHATEVER THE USER HAS SELECTED OF DATE, NUMBER, AND COST. FOR EACH SELECTION IF DATE
+        //HAS BEEN SELECTED PASS IN NULL FOR THE OTHERS UNTIL THEY ARE SELECTED THEN YOU GET THAT
+        //FILTERING AFFECT.
+
+
     }
 }
